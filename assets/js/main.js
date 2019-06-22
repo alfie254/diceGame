2ed9b755f4799985e0de8bf017ac394608a98dd6
alfie254 <alfredwmaina25@gmail.com>
2019-06-22 15:57:22 +0300

    roll dice enabled

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,9 @@ var score, activePlayer, currentScore;
 init();
 
 function init(){
+  score = [0, 0];
+  activePlayer = 0;
+  currentScore = 0;
   document.querySelector('#score-0').innerHTML = 0;
   document.querySelector('#score-1').innerHTML = 0;
   document.querySelector('#current-0').innerHTML = 0;  
@@ -17,10 +20,20 @@ function init(){
 }
 
 function hideDice(){  
-  document.querySelector('#dice-1').style.display = 'none';
-  document.querySelector('#dice-2').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 }
 function showDice(){  
-  document.querySelector('#dice-1').style.display = 'block';
-  document.querySelector('#dice-2').style.display = 'block';
+  document.getElementById('dice-1').style.display = 'block';
+  document.getElementById('dice-2').style.display = 'block';
 }
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  var dice1 = Math.floor(Math.random() * 6 + 1);
+  var dice2 = Math.floor(Math.random() * 6 + 1);
+
+  document.getElementById('dice-1').src = 'assets/images/dice-' + dice1 + '.png';  
+  document.getElementById('dice-2').src = 'assets/images/dice-' + dice2 + '.png';
+  showDice();
+  
+});
